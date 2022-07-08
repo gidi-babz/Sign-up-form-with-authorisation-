62226fe6e4449f9724b928dbc8b5cfac21615e08
Gidi Babz <tunex4ind@gmail.com>
2022-07-08 11:15:11 +0100

    fresh commits

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -20,37 +20,45 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDS2dzzOPEBXiUv585k99-_JQefVaz4bGg';
     } else {
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDS2dzzOPEBXiUv585k99-_JQefVaz4bGg',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      ).then(res => {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDS2dzzOPEBXiUv585k99-_JQefVaz4bGg';
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
         setIsLoading(false);
         if (res.ok) {
-          // ...do somethig
+          return res.json();
         } else {
           return res.json().then(data => {
-            console.log(data);
             let errorMessage = 'Authentication failed';
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        alert(error.message);
       });
-    }
   };
 
   return (
